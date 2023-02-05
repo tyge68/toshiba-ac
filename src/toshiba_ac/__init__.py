1cabc7f036fbf1f41609bdd3ae972a57957eb805
Thierry Ygé <tyge@adobe.com>
2023-02-05 20:52:02 +0100

    chore: export generator defined classes

--- src/toshiba_ac/__init__.py
+++ src/toshiba_ac/__init__.py
@@ -1,5 +1,5 @@
 import sys
-
+from .generator import *
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
